9b498218a17d2ca71b5720216194c426fe41552f
Paulo Cardoso <paulo_jcs@yahoo.com.br>
2020-07-21 20:29:19 -0300

    deploy

--- delivery/settings.py
+++ delivery/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['127.0.0.1','delverysl.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','sldelivery.herokuapp.com']
 
 
 # Application definition
